eff9db4bf58ddd79028fe44c3752e172240e9530
Yuta Kanayama <kanayama.work@gmail.com>
2015-11-11 10:47:00 +0900

    fix

--- htdocs/js/router.js
+++ htdocs/js/router.js
@@ -1,12 +1,16 @@
 var Router = Backbone.Router.extend({
+  
   routes: {
     '': 'index',
     ':id': 'category'
   },
+  
   index: function(){
     // ハッシュなしでアクセスされたときの処理を書く
   },
+  
   category: function(id){
     this.trigger('transition', id);
   }
+  
 });
